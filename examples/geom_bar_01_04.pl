8faad1f1d68b14158682e36393e07fe73516c1ee
stphnlyd <stephanloyd9@gmail.com>
2019-02-10 02:06:26 +0800

    more geom_bar() examples

---
+++ examples/geom_bar_01_04.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use 5.016;
+use warnings;
+
+use Getopt::Long;
+use Chart::GGPlot::Functions qw(:all);
+use Data::Frame::More::Examples qw(mpg);
+
+my $save_as;
+GetOptions( 'o=s' => \$save_as );
+
+my $mpg = mpg();
+
+my $p = ggplot(
+    data    => $mpg,
+    mapping => aes( x => 'class' )
+)->geom_bar( mapping => aes( fill => 'drv' ), position => 'dodge' );
+
+if (defined $save_as) {
+    $p->save($save_as);
+} else {
+    $p->show();
+}
+
