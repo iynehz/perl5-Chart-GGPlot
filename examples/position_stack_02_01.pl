b65add38c63025239bf481f5014947c94a400a65
stphnlyd <stephanloyd9@gmail.com>
2019-02-10 19:12:02 +0800

    examples for position_stack() and position_fill()

---
+++ examples/position_stack_02_01.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use 5.016;
+use warnings;
+
+use Getopt::Long;
+use Chart::GGPlot::Functions qw(:all);
+use Data::Frame::More::Examples qw(diamonds);
+
+my $save_as;
+GetOptions( 'o=s' => \$save_as );
+
+my $p = ggplot(
+    data    => diamonds(),
+    mapping => aes( x => 'price', fill => 'cut' )
+)->geom_histogram( binwidth => 500 );
+
+if ( defined $save_as ) {
+    $p->save($save_as);
+}
+else {
+    $p->show();
+}
+
